ed410c21076f858f39f38fc893323eec13e09f3c
lucidmaj7 <lucidmaj7@gmail.com>
2020-08-27 23:15:45 +0900

    네이버 로그인 화면 카피

---
+++ react_study/like_button.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const element = <h1>Hello, world</h1>;
+
+
+
+// ... the starter code you pasted ...
+
+
+ReactDOM.render(element, document.getElementById('root'));
+//ReactDOM.render(e, document.getElementById('root'));
