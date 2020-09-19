5c308a49627b5be07e814fdd678fae6d5c23f3a2
lucidmaj7 <lucidmaj7@gmail.com>
2020-09-19 13:31:39 +0900

    react ajax 테스트

--- react/ajaxtest/src/App.js
+++ react/ajaxtest/src/App.js
@@ -1,22 +1,84 @@
-import React from 'react';
+import React, { useEffect ,useState} from 'react';
+
+function Nav(props){
+  let [list,setList] = useState([]);
+  let [isFetch,setIsFetch] = useState(false);
+
+  useEffect(()=>{
+    if(isFetch===false)
+    { 
+      fetch('list.json')
+      .then((res)=>{
+        return res.json();
+      }).then((json)=>{
+        console.log("fetch");
+        setIsFetch(true);
+        setList(json);
+      });
+    }
+  },);
+
+  let navlist = [];
+
+  for(let i = 0;i<list.length;i++)
+  {
+    navlist.push(<li key={list[i].id} ><a href={list[i].id} onClick={(e)=>{
+      e.preventDefault();
+      props.onClick(list[i].id);
 
 
+    }}> {list[i].title}</a></li>);
+  }
+
+  return(
+  <nav>
+    <ul>
+    {navlist}
+    </ul>
+  </nav>
+  );
+}
+
+function Article(props){
+  let [title,setTitle] = useState(props.title);
+  let [desc,setDesc] = useState(props.desc);
+
+
+  useEffect(()=>{
+    if(props.id!==0)
+    { 
+      fetch(`${props.id}.json`)
+      .then((res)=>{
+        return res.json();
+      }).then((json)=>{
+        console.log("Article Fetched!");
+        setTitle(json.title);
+        setDesc(json.desc);
+      }).catch(()=>{
+        setTitle(`${props.id} is not found`);
+        setDesc("");
+      });
+    }
+  },[props.id]);
+
+  return(
+    <article>
+      <h2>{title}</h2>
+      <p>{desc}</p>
+    </article>
+  );
+}
 
 function App() {
+  let [selected , setSelect]=useState(0);
   return (
     <div className="App">
       <h1>WEB</h1>
-    <nav>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-      </ul>
-    </nav>
-    <article>
-      <h2>Welcome</h2>
-        <p>Hello, React & Ajax</p>
-    </article>
+      <Nav onClick={(id)=>{
+          console.log(`id is clicked ${id}`);
+          setSelect(id);
+      }}></Nav>
+      <Article id={selected} title={"Welcome"} desc={"Hello React~"}></Article>
     </div>
   );
 }
