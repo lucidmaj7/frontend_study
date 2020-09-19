827e73b45d1ffecde07e1e9074c7745a186f6891
lucidmaj7 <lucidmaj7@gmail.com>
2020-09-19 12:41:39 +0900

    react ajax 테스트 추가

---
+++ react/ajaxtest/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+    <nav>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+      </ul>
+    </nav>
+    <article>
+      <h2>Welcome</h2>
+        <p>Hello, React & Ajax</p>
+    </article>
+    </div>
+  );
+}
+
+export default App;
