fbc05423c3026471bdbcc2df881706c5f0a1c40e
root <root@localhost.localdomain>
2020-10-23 16:44:32 +0900

    add react-redux-todo list project

---
+++ redux-todolist/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+
+
+
+const App = ()=> {
+  return (
+  	<div>
+		<h1>to do list </h1>	
+		<Header today="hello-world"></Header>
+	</div> 
+  );
+}
+
+export default App;
